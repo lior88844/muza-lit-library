e5096804e0c1982eea49c8a4c17a685c7bc866c9
drznr <dresnerdaniel@gmail.com>
2025-10-29 12:33:55 +0200

    Revert "init artist page and load artist data"
    This reverts commit c5efcc841d94d2dc68884f90c3a69f49e67ed33b.

--- app/routes.ts
+++ app/routes.ts
@@ -1,5 +1,4 @@
 import { index, route, type RouteConfig } from '@react-router/dev/routes'
-
 const resourcesRoutes = [
   route('/health', './routes/resources/health.ts'),
   route('/api/library', './routes/resources/library.ts'),
