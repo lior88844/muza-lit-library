511f12302886a3b33dc3d0cd874bdb8106c75a4f
drznr <dresnerdaniel@gmail.com>
2025-10-29 12:34:01 +0200

    Revert "init artist page and load artist data"
    This reverts commit 3b01cabd2e2058257e8937a4c3998fcaa4b5bbce.

--- app/routes/artist.tsx
+++ app/routes/artist.tsx
@@ -1,49 +1,5 @@
-import { isRouteErrorResponse, useRouteError } from 'react-router'
-import { useLoaderData } from 'react-router'
-import { fetchArtistById } from 'server/data'
+// @TODO artist page details
 
-export async function loader({ params }: { params: { id: string } }) {
-  const artistId = +params.id
-
-  if (isNaN(artistId)) {
-    throw new Response('Invalid artist ID', { status: 400 })
-  }
-
-  const artistData = await fetchArtistById(artistId)
-
-  if (!artistData) {
-    throw new Response('Artist not found', { status: 404 })
-  }
-
-  return { artist: artistData }
-}
-
-export default function ArtistPage() {
-  const { artist } = useLoaderData<typeof loader>()
-
-  return <main>Artist PAGE DETAILS {artist.name}</main>
-}
-
-export function ErrorBoundary() {
-  const error = useRouteError()
-
-  if (isRouteErrorResponse(error)) {
-    const message =
-      typeof error.data === 'string'
-        ? error.data
-        : (error.data?.message ?? error.statusText ?? 'Error')
-    return (
-      <div>
-        <h2>{error.status}</h2>
-        <p>{message}</p>
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      <h2>Something went wrong</h2>
-      <p>Try again later.</p>
-    </div>
-  )
+export default function Artist() {
+  return <div>Artist PAGE DETAILS</div>
 }
